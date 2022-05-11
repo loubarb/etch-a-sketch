442242972aea71d1e3a398948259478ed154e2e7
loubarb <louis.barbati@gmail.com>
2022-05-11 00:03:27 -0400

    Added function to control arrow keys to control drawing

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const height = canvas.height;
 
 let xStartPoint = Math.floor(Math.random() * width);
 let yStartPoint = Math.floor(Math.random() * height);
+console.log(xStartPoint, yStartPoint);
 
 ctx.linJoin = 'round';
 ctx.lineCap = 'round';
@@ -15,3 +16,39 @@ ctx.moveTo(xStartPoint, yStartPoint);
 ctx.lineTo(xStartPoint, yStartPoint);
 ctx.stroke();
 
+function draw({key}) {
+  if (key === 'ArrowUp') {
+    ctx.beginPath();
+    ctx.moveTo(xStartPoint, yStartPoint);
+    yStartPoint = yStartPoint - 10;
+    ctx.lineTo(xStartPoint, yStartPoint);
+    ctx.stroke();
+  } else if (key === 'ArrowDown') {
+    ctx.beginPath();
+    ctx.moveTo(xStartPoint, yStartPoint);
+    yStartPoint = yStartPoint + 10;
+    ctx.lineTo(xStartPoint, yStartPoint);
+    ctx.stroke();
+  } else if (key === 'ArrowLeft') {
+    ctx.beginPath();
+    ctx.moveTo(xStartPoint, yStartPoint);
+    xStartPoint = xStartPoint - 10;
+    ctx.lineTo(xStartPoint, yStartPoint);
+    ctx.stroke();
+  } else if (key === 'ArrowRight') {
+    ctx.beginPath();
+    ctx.moveTo(xStartPoint, yStartPoint);
+    xStartPoint = xStartPoint + 10;
+    ctx.lineTo(xStartPoint, yStartPoint);
+    ctx.stroke();
+  }
+}
+
+function handleKey(e) {
+  console.log(e.key);
+  if (e.key === 'ArrowUp' || 'ArrowDown' || 'ArrowLeft' || 'ArrowRight') {
+    draw({key: e.key});
+  }
+}
+
+window.addEventListener('keydown', handleKey)
